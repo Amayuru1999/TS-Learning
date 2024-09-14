1971f89c0a8f888289594aa055c74d693fa96eb3
Amayuru1999 <amarasingheau@gmail.com>
2024-09-14 16:46:30 +0530

    feat: added enums and interfaces

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,47 @@ Math.round(variable)
 console.log('Variable:', variable);
 
 //Arrays
+
+const names: string[]=[];
+names.push('Amayuru');
+console.log('✳️ Names:', names);
+
+
+//Objects
+const person: {name: string, age: number} = {
+    name: 'Amayuru',
+    age: 25
+};
+console.log('😊 Person:', person);
+
+//Enums
+enum CardinalDirections {
+    North,
+    East,
+    South,
+    West
+}
+const cardinalDirection: CardinalDirections = CardinalDirections.East;
+console.log('🟩🟩 Cardinal Direction:', cardinalDirection);
+
+
+//Interfaces
+interface Person {
+    name: string;
+    age: number;
+}
+
+const newPerson : Person = {
+    name: 'Amayuru',
+    age: 25
+};
+
+console.log('😜😜 New Person:', newPerson);
+
+//Functions
+function getTime(): number {
+    const date= Date.now();
+
+    return date;
+}
+console.log('⌚ Time:', getTime());
