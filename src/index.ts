2dcbe2907a35b9bf15fd25f1c976bff1b8843468
Amayuru1999 <amarasingheau@gmail.com>
2024-09-14 15:58:21 +0530

    init: setup project with ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+console.log('Hello, world! ❌❌❌');
+console.log('Hello, world! ✅✅✅');
+
+let variable: any = true;
+variable = "false";
+Math.round(variable)
+
+console.log('Variable:', variable);
+
+//Arrays
